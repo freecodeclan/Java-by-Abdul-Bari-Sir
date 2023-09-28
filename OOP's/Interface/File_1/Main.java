cfc3088168c49dd1f148b2d37fd78fbf6af1f896
Harsh <harshmehra53@gmail.com>
2023-09-28 08:36:39 +0530

    update

---
+++ OOP's/Interface/File_1/Main.java
@@ -0,0 +1,9 @@
+package Interface.File_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Test t = new My();
+        t.method1();
+        t.method2();
+    }
+}
---
+++ OOP's/Interface/File_1/Test.java
@@ -0,0 +1,6 @@
+package Interface.File_1;
+
+interface Test {
+    void method1();
+    void method2();
+}
