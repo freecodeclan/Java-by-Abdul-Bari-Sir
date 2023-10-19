4a76db498269095bf1ff43bdebfba2da6da53ac0
Harsh <harshmehra53@gmail.com>
2023-10-19 20:45:16 +0530

    up

---
+++ CollectionFramework/Map/Hashmapques.java
@@ -0,0 +1,17 @@
+/* Question 2 --- Given an array, find the most frequent element in it. If thewre are multiple elements 
+ that appear a maximum number of times, print anyone of them.
+
+ n = 6;
+ arr[] = {1,3,2,1,4,1}
+
+ Expected outpot : 1
+
+ */
+
+package Map;
+import java.util.*;
+public class Hashmapques {
+    public static void main(String[] args) {
+        
+    }
+}
